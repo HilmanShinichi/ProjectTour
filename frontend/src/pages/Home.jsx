05f2c118404986489f6f0c168c22a704917df16a
Hilman Shinichi <hilmanseptiana19@gmail.com.com>
2023-05-25 22:36:26 +0700

    hari pertama malam

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,8 +6,9 @@ import heroImg from "../assets/images/hero-img01.jpg";
 import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.png";
-
 import Subtitle from "../shared/Subtitle";
+import SearchBar from "../shared/SearchBar";
+import ServiceList from "../services/ServiceList";
 
 const Home = () => {
   return (
@@ -29,10 +30,39 @@ const Home = () => {
                 </p>
               </div>
             </Col>
+
+            <Col lg='2'>
+             <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+             </div>
+            </Col>
+            <Col lg='2'>
+             <div className="hero__img-box mt-4">
+                <video src={heroVideo} alt="" controls />
+             </div>
+            </Col>
+            <Col lg='2'>
+             <div className="hero__img-box mt-5 ">
+                <img src={heroImg02} alt="" />
+             </div>
+            </Col>
           </Row>
+
+          <SearchBar/>
         </Container>
       </section>
       {/* ========= Section Asset HeroImg End ======== */}
+      <section>
+        <Container> 
+          <Row>
+            <Col lg='3'>
+              <h5 className="services__subtitle">What we serve</h5>
+              <h2 className="services__title">We offer our best services</h2>
+            </Col>
+            <ServiceList/>
+          </Row>
+        </Container>
+      </section>
     </>
   );
 };
