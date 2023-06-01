856856ac68f786cef05cd93a07bfee04618ff3eb
Hilman Shinichi <hilmanseptiana19@gmail.com.com>
2023-06-01 23:27:36 +0700

    hari kedelapan jam 2335

--- frontend/src/shared/SearchBar.jsx
+++ frontend/src/shared/SearchBar.jsx
@@ -1,58 +1,90 @@
-import React, {useRef} from 'react';
-import './SearchBar.css';
+import React, { useRef } from "react";
+import "./SearchBar.css";
 
-import { Col, Form, FormGroup } from "reactstrap"; 
+import { Col, Form, FormGroup } from "reactstrap";
 
+//panggil baseURL sebagai endpoint API
+import { BASE_URL } from "./../utils/config";
+
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+  const locationRef = useRef("");
+  const distanceRef = useRef(0);
+  const maxGroupSizeRef = useRef(0);
+  const navigate = useNavigate();
+  // linkref  digunakan untuk mentarget element untuk component
+  // ref sama halnya refrensi ketika di klik maka target ke refrensi
+  const searchHandler = async () => {
+    const location = locationRef.current.value;
+    const distance = distanceRef.current.value;
+    const maxGroupSize = maxGroupSizeRef.current.value;
 
-    const locationRef = useRef('')
-    const distanceRef = useRef(0)
-    const maxGroupSizeRef = useRef(0)
-    // linkref  digunakan untuk mentarget element untuk component 
-    // ref sama halnya refrensi ketika di klik maka target ke refrensi
-    const searchHandler = () => {
-        const location = locationRef.current.value
-        const distance = distanceRef.current.value
-        const maxGroupSize = maxGroupSizeRef.current.value
-
-        if(location === '' || distance === '' ||  maxGroupSize === ''){
-            return alert("All fields are required!")
-        }
+    if (location === "" || distance === "" || maxGroupSize === "") {
+      return alert("All fields are required!");
     }
 
-  return <Col lg='12'>
-    <div className='search__bar'>
-        <Form className='d-flex align-items-center gap-4'>
-            <FormGroup className='d-flex gap-3 form__group form__group-fast'>
-                <span><i className='ri-map-pin-line'></i></span>
-                <div>
-                    <h6>Location</h6>
-                    <input type="text" placeholder='Where are you going ??' ref={locationRef} />
-                </div>
-            </FormGroup>
-            <FormGroup className='d-flex gap-3 form__group form__group-fast'>
-                <span><i className='ri-map-pin-line'></i></span>
-                <div>
-                    <h6>Distance</h6>
-                    <input type="number" placeholder='Distance k/m' ref={distanceRef
-                    } />
-                </div>
-            </FormGroup>
-            <FormGroup className='d-flex gap-3 form__group form__group-last'>
-                <span><i className='ri-group-line'></i></span>
-                <div>
-                    <h6>Max People</h6>
-                    <input type="number" placeholder='0' ref={maxGroupSizeRef} />
-                </div>
-            </FormGroup>
+    const res = await fetch(
+      `${BASE_URL}/tours/search/getToursBySearch?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`
+    );
 
-            <span  className='search__icon' type="submit" onClick={searchHandler}>
-                <i className='ri-search-line'></i>
+    if (!res.ok) alert("Something went wrong");
+
+    const result = await res.json();
+
+    navigate(
+      `/tours/search?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`,
+      { state: result.data }
+    );
+  };
+
+  return (
+    <Col lg="12">
+      <div className="search__bar">
+        <Form className="d-flex align-items-center gap-4">
+          <FormGroup className="d-flex gap-3 form__group form__group-fast">
+            <span>
+              <i className="ri-map-pin-line"></i>
             </span>
-        </Form>
-    </div>
-  </Col>
-}
+            <div>
+              <h6>Location</h6>
+              <input
+                type="text"
+                placeholder="Where are you going ??"
+                ref={locationRef}
+              />
+            </div>
+          </FormGroup>
+          <FormGroup className="d-flex gap-3 form__group form__group-fast">
+            <span>
+              <i className="ri-map-pin-line"></i>
+            </span>
+            <div>
+              <h6>Distance</h6>
+              <input
+                type="number"
+                placeholder="Distance k/m"
+                ref={distanceRef}
+              />
+            </div>
+          </FormGroup>
+          <FormGroup className="d-flex gap-3 form__group form__group-last">
+            <span>
+              <i className="ri-group-line"></i>
+            </span>
+            <div>
+              <h6>Max People</h6>
+              <input type="number" placeholder="0" ref={maxGroupSizeRef} />
+            </div>
+          </FormGroup>
 
-export default SearchBar
+          <span className="search__icon" type="submit" onClick={searchHandler}>
+            <i className="ri-search-line"></i>
+          </span>
+        </Form>
+      </div>
+    </Col>
+  );
+};
+
+export default SearchBar;
